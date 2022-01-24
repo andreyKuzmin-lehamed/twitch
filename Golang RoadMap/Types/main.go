058c35e6a77ecdd5dc55f30be67713b098affa36
andreyKuzmin-lehamed <vrges.lama@gmail.com>
2022-01-24 15:52:59 -0500

    add golang roadmap types info

--- Golang RoadMap/Types/main.go
+++ Golang RoadMap/Types/main.go
@@ -5,6 +5,39 @@ import (
 )
 
 func main() {
+	fmt.Println("Обращаемся к символам строки, как к slice с байтами.")
+	var str string = "Hello"
+	fmt.Println("Вся строка:", str)
+	fmt.Println("Байты через Printf:")
+	fmt.Printf("% x\n", str)
+	for i := 0; i < len(str); i++ {
+		fmt.Println("Байт:", str[i], "Строковый тип:", string(str[i]))
+	}
+
+	fmt.Println("Но обращаясь к кириллице получаем проблемку.")
+	var str_cir string = "Привет"
+	fmt.Println("Вся строка:", str_cir)
+	fmt.Println("Получая байты через Printf, видим, что их больше чем символов:")
+	fmt.Printf("% x\n", str_cir)
+	fmt.Println("Теперь немного изменяем Printf, чтобы получать юникод:")
+	fmt.Printf("%+q\n", str_cir)
+	for i := 0; i < len(str_cir); i++ {
+		fmt.Println("Байт:", str_cir[i], "Строковый тип:", string(str_cir[i]))
+	}
+
+	fmt.Println("Конвертнем из []byte в []rune, и пробежися ещё раз:")
+	str_cir_runes := []rune(str_cir)
+	for i := 0; i < len(str_cir_runes); i++ {
+		fmt.Println("Руна:", str_cir_runes[i], "Строковый тип:", string(str_cir_runes[i]))
+	}
+
+	fmt.Println("Переберём строку с кириллицей через rune, тут без проблем.")
+	for _, r := range str_cir {
+		fmt.Println("Руна:", r, "Строковый тип:", string(r))
+	}
+}
+
+func strLiterals() {
 	str := "🔴"
 	// fmt.Println(str)
 	byte_count := 0
